7794e95f301d2e9b6485f0bca72421b8d2bcb7cc
jacques Vidjanagni <jacquesvidja@gmail.com>
2026-03-22 21:21:08 -0400

    lab07

--- week9/lab06_100989148/exercise-1/hello-world.js
+++ week9/lab06_100989148/exercise-1/hello-world.js
@@ -1,6 +1,1 @@
-var greeter = function (firstName, lastName) {
-    return "Hello, ".concat(firstName, " ").concat(lastName);
-};
-var firstName = "Jacques";
-var lastName = "Vidja";
-console.log(greeter(firstName, lastName));
+console.log("Hello World");
